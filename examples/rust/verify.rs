fa0a3534a1baa38894630d67fdd2b8e5cfa0e558
Akshat Mahajan <akshat@cloudflare.com>
2025-06-07 17:46:40 -0400

    Quick bugfix in examples

--- examples/rust/verify.rs
+++ examples/rust/verify.rs
@@ -1,6 +1,6 @@
 use web_bot_auth::{
     SignatureAgentLink, WebBotAuthSignedMessage, WebBotAuthVerifier,
-    components::{CoveredComponent, DerivedComponent},
+    components::{CoveredComponent, DerivedComponent, HTTPField},
     keyring::KeyRing,
     message_signatures::SignedMessage,
 };
@@ -9,15 +9,21 @@ struct MySignedMsg;
 
 impl SignedMessage for MySignedMsg {
     fn fetch_signature_header(&self) -> Option<String> {
-        Some("sig1=:uz2SAv+VIemw+Oo890bhYh6Xf5qZdLUgv6/PbiQfCFXcX/vt1A8Pf7OcgL2yUDUYXFtffNpkEr5W6dldqFrkDg==:".to_owned())
+        Some("sig1=:GXzHSRZ9Sf6WwLOZjxAhfE6WEUPfDMrVBJITsL2sbG8gtcZgqKe2Yn7uavk0iNQrfcPzgGq8h8Pk5osNGqdtCw==:".to_owned())
     }
     fn fetch_signature_input(&self) -> Option<String> {
-        Some(r#"sig1=("@authority");created=1735689600;keyid="poqkLGiymh_W0uP6PZFw-dvez3QJT5SolqXBCW38r0U";alg="ed25519";expires=1735693200;nonce="gubxywVx7hzbYKatLgzuKDllDAIXAkz41PydU7aOY7vT+Mb3GJNxW0qD4zJ+IOQ1NVtg+BNbTCRUMt1Ojr5BgA==";tag="web-bot-auth""#.to_owned())
+        Some(r#"sig1=("@authority" "signature-agent");alg="ed25519";keyid="poqkLGiymh_W0uP6PZFw-dvez3QJT5SolqXBCW38r0U";nonce="ZO3/XMEZjrvSnLtAP9M7jK0WGQf3J+pbmQRUpKDhF9/jsNCWqUh2sq+TH4WTX3/GpNoSZUa8eNWMKqxWp2/c2g==";tag="web-bot-auth";created=1749332605;expires=1749332615"#.to_owned())
     }
     fn lookup_component(&self, name: &CoveredComponent) -> Option<String> {
-        match *name {
+        match name {
             CoveredComponent::Derived(DerivedComponent::Authority { .. }) => {
                 Some("example.com".to_string())
+            }
+            CoveredComponent::HTTP(HTTPField { name, .. }) => {
+                if name == "signature-agent" {
+                    return Some(String::from("\"https://myexample.com\""));
+                }
+                None
             }
             _ => None,
         }
@@ -26,15 +32,17 @@ impl SignedMessage for MySignedMsg {
 
 impl WebBotAuthSignedMessage for MySignedMsg {
     fn fetch_all_signature_agents(&self) -> Vec<String> {
-        vec!["\"https://mydata.com\"".into()]
+        vec!["\"https://myexample.com\"".into()]
     }
 }
 
 fn main() {
     // Verifying a Web Bot Auth message
-    let public_key = [0x26, 0xb4, 0x0b, 0x8f, 0x93, 0xff, 0xf3, 0xd8, 0x97, 0x11, 0x2f, 0x7e, 0xbc, 0x58, 0x2b,
+    let public_key = [
+        0x26, 0xb4, 0x0b, 0x8f, 0x93, 0xff, 0xf3, 0xd8, 0x97, 0x11, 0x2f, 0x7e, 0xbc, 0x58, 0x2b,
         0x23, 0x2d, 0xbd, 0x72, 0x51, 0x7d, 0x08, 0x2f, 0xe8, 0x3c, 0xfb, 0x30, 0xdd, 0xce, 0x43,
-        0xd1, 0xbb];
+        0xd1, 0xbb,
+    ];
     let mut keyring = KeyRing::default();
     keyring.import_raw(
         "poqkLGiymh_W0uP6PZFw-dvez3QJT5SolqXBCW38r0U".to_string(),
@@ -46,7 +54,7 @@ fn main() {
     for url in verifier.get_signature_agents().iter() {
         assert_eq!(
             url,
-            &SignatureAgentLink::External("https://mydata.com".into())
+            &SignatureAgentLink::External("https://myexample.com".into())
         )
     }
     // Since the expiry date is in the past.
